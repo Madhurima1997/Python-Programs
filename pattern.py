d7329d53da31703023845af5f4f89cbf22088058
Madhurima Chakraborty <61430343+Madhurima1997@users.noreply.github.com>
2020-04-21 01:03:28 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,7 @@
+n=int(input()) #takes input from user
+l=[str(x) for x in range(n*2) if x%2!=0] #generates list of odd numbers using list comprehension
+for _ in range(n):
+    print(''.join(l)) # generating the pattern
+    x=l[0]
+    l.remove(x)
+    l.append(x)
